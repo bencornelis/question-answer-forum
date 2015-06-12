52222a4852e215c410d3b611d171f1794c61bfeb
Ben Cornelis <student@epicodus.com>
2015-06-12 09:42:10 -0700

    add questions, new-questions templates; allow user to ask a new question

---
+++ controllers/new-question.js
@@ -0,0 +1,18 @@
+App.NewQuestionController = Ember.Controller.extend({
+  actions: {
+    addQuestion: function() {
+      var newQuestion = this.store.createRecord('question', {
+        question: this.get('question'),
+        author: this.get('author'),
+        description: this.get('description')
+      });
+      newQuestion.save();
+
+      this.set('question', '');
+      this.set('author', '');
+      this.set('description', '');
+
+      this.transitionToRoute('questions');
+    }
+  }
+});
--- router.js
+++ router.js
@@ -1,3 +1,5 @@
 App.Router.map(function() {
-  this.resource('questions', {path: '/'});
+  this.resource('questions', {path: '/'}, function() {
+    this.resource('new-question', {path: 'questions/new'});
+  });
 })
